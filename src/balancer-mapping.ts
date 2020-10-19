b387673639b53a811b5a7a6ba84eb6b5c28be423
benesjan <janbenes1234@gmail.com>
2020-10-19 15:08:04 +0200

    Type conversion bug fix

--- src/balancer-mapping.ts
+++ src/balancer-mapping.ts
@@ -1,15 +1,16 @@
+import {Address} from "@graphprotocol/graph-ts"
 import {Transfer} from "../generated/BalTokenContract/ERC20";
 import {Reward} from "../generated/schema";
 
+let txOriginator = Address.fromString("0xcb3d284995e2ed50f8e9971718f2166a95ff83c2")
+let disperseApp = Address.fromString("0xd152f549545093347a162dce210e7293f1452150")
+
 
 export function handleTransfer(event: Transfer): void {
-    if (event.transaction.from.toString() ===
-        "0xcb3d284995e2ed50f8e9971718f2166a95ff83c2" &&
-        event.params.from.toString() ===
-        "0xd152f549545093347a162dce210e7293f1452150") {
+    if (event.transaction.from == txOriginator && event.params.from == disperseApp) {
         // Filter for events which were emitted in a tx originating from
         // BAL team and the event's from address has to point to Disperse.app
-        let reward = new Reward(event.params.to.toString() + "-" + event.block.timestamp.toString())
+        let reward = new Reward(event.params.to.toHexString() + "-" + event.block.timestamp.toString())
         reward.exchange = "BALANCER"
         reward.amount = event.params.value
         reward.user = event.params.to
