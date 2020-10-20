72e7bdbd21bd224857f1642015214e64d3b7319d
benesjan <janbenes1234@gmail.com>
2020-10-20 17:32:29 +0200

    StakedEvent bug fix

--- src/uni-v2-mapping.ts
+++ src/uni-v2-mapping.ts
@@ -1,28 +1,35 @@
-import {Address} from "@graphprotocol/graph-ts"
+import {Address, log} from "@graphprotocol/graph-ts"
 import {Reward, StakedEvent, WithdrawnEvent} from "../generated/schema";
 import {
     RewardPaid,
-    Staked, Withdrawn
+    Staked,
+    Withdrawn
 } from "../generated/UniStakingRewards/StakingRewards";
 
-// The following syntax is not supported - probably outdated typescript version in graph-cli
-// let poolPairMap = new Map<string, string>([
-//     ["0xca35e32e7926b96a9988f61d510e038108d8068e", "0xbb2b8038a1640196fbe3e38816f3e67cba72d940"],
-//     ["0x7fba4b8dc5e7616e59622806932dbea72537a56b", "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc"],
-//     ["0x6c3e4cb2e96b01f4b866965a91ed4437839a121a", "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852"],
-//     ["0xa1484c3aa22a66c62b77e0ae78e15258bd0cb711", "0xa478c2975ab1ea89e8196811f51a7b7ade33eb11"],
-// ])
 
-let poolPairMap = new Map<string, string>()
-poolPairMap.set("0xca35e32e7926b96a9988f61d510e038108d8068e", "0xbb2b8038a1640196fbe3e38816f3e67cba72d940")
-poolPairMap.set("0x7fba4b8dc5e7616e59622806932dbea72537a56b", "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc")
-poolPairMap.set("0x6c3e4cb2e96b01f4b866965a91ed4437839a121a", "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852")
-poolPairMap.set("0xa1484c3aa22a66c62b77e0ae78e15258bd0cb711", "0xa478c2975ab1ea89e8196811f51a7b7ade33eb11")
+// For some reason I could not get map working properly, so I am using this function
+// as a workaround
+function getPool(address: Address | null): Address | null {
+    if (address == Address.fromString("0xca35e32e7926b96a9988f61d510e038108d8068e")) {
+        return Address.fromString("0xbb2b8038a1640196fbe3e38816f3e67cba72d940")
+    }
+    if (address == Address.fromString("0x7fba4b8dc5e7616e59622806932dbea72537a56b")) {
+        return Address.fromString("0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc")
+    }
+    if (address == Address.fromString("0x6c3e4cb2e96b01f4b866965a91ed4437839a121a")) {
+        return Address.fromString("0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852")
+    }
+    if (address == Address.fromString("0xa1484c3aa22a66c62b77e0ae78e15258bd0cb711")) {
+        return Address.fromString("0xa478c2975ab1ea89e8196811f51a7b7ade33eb11")
+    }
+    log.warning("Unknown pool address", []);
+    return null
+}
 
 export function handleRewardPaid(event: RewardPaid): void {
     let reward = new Reward(event.params.user.toHexString() + "-" + event.logIndex.toString())
     reward.exchange = "UNI_V2"
-    reward.pool = Address.fromString(poolPairMap.get(event.transaction.to.toHexString()))
+    reward.pool = getPool(event.transaction.to)
     reward.amount = event.params.reward
     reward.user = event.params.user
     reward.transaction = event.transaction.hash
@@ -34,7 +41,7 @@ export function handleRewardPaid(event: RewardPaid): void {
 export function handleStaked(event: Staked): void {
     let staked = new StakedEvent(event.params.user.toHexString() + "-" + event.logIndex.toString())
     staked.exchange = "UNI_V2"
-    staked.pool = Address.fromString(poolPairMap.get(event.transaction.to.toHexString()))
+    staked.pool = getPool(event.transaction.to)
     staked.amount = event.params.amount
     staked.user = event.params.user
     staked.transaction = event.transaction.hash
@@ -46,7 +53,7 @@ export function handleStaked(event: Staked): void {
 export function handleWithdrawn(event: Withdrawn): void {
     let withdrawn = new WithdrawnEvent(event.params.user.toHexString() + "-" + event.logIndex.toString())
     withdrawn.exchange = "UNI_V2"
-    withdrawn.pool = Address.fromString(poolPairMap.get(event.transaction.to.toHexString()))
+    withdrawn.pool = getPool(event.transaction.to)
     withdrawn.amount = event.params.amount
     withdrawn.user = event.params.user
     withdrawn.transaction = event.transaction.hash
