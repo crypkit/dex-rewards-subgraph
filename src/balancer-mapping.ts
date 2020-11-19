8f6467c8f2879c0986b7b680b8f4704e652a9ad7
benesjan <janbenes1234@gmail.com>
2020-11-19 16:36:33 -0600

    Updated subgraph to the new BAL distribution scheme

--- src/balancer-mapping.ts
+++ src/balancer-mapping.ts
@@ -2,6 +2,7 @@ import {Address} from "@graphprotocol/graph-ts"
 import {Transfer} from "../generated/BalTokenContract/ERC20";
 import {Reward} from "../generated/schema";
 import {BigDecimal} from "@graphprotocol/graph-ts/index";
+import {Claimed} from "../generated/BalMerkleRedeem/MerkleRedeem";
 
 let txOriginator = Address.fromString("0xcb3d284995e2ed50f8e9971718f2166a95ff83c2")
 let disperseApp = Address.fromString("0xd152f549545093347a162dce210e7293f1452150")
@@ -26,4 +27,21 @@ export function handleTransfer(event: Transfer): void {
         reward.blockTimestamp = event.block.timestamp
         reward.save()
     }
+}
+
+export function handleClaimed(event: Claimed): void {
+    // Filter for events which were emitted in a tx originating from
+    // BAL team and the event's from address has to point to Disperse.app
+    let id = event.params._claimant
+        .toHexString()
+        .concat('-')
+        .concat(event.logIndex.toString())
+    let reward = new Reward(id)
+    reward.exchange = "BALANCER"
+    reward.amount = event.params._balance.toBigDecimal().times(DENOMINATION)
+    reward.user = event.params._claimant
+    reward.transaction = event.transaction.hash
+    reward.blockNumber = event.block.number
+    reward.blockTimestamp = event.block.timestamp
+    reward.save()
 }
